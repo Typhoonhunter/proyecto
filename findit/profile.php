a01acf11a35926c90f70ef19f5d6c16cad89f5e2
David HC <a01205020@itesm.mx>
2015-03-21 14:32:24 -0600

    Modificación de funciones.php
    Falta agregar funcionalidad a botón "log out" y el manejo de sesiones.

--- findit/profile.php
+++ findit/profile.php
@@ -1,8 +1,9 @@
+<!DOCTYPE html>
+
 <?php
-    include_once("funciones.php");
+	include_once("funciones.php");
 ?>
 
-<!DOCTYPE html>
 <html lang="en">
 
     <head>
@@ -22,22 +23,30 @@
         <link href="color/default.css" rel="stylesheet">
 
     </head>
-    
+
     <body id="page-top" data-spy="scroll" data-target=".navbar-custom">
-	<!-- Preloader -->
+        <!-- Preloader -->
         <div id="preloader">
             <div id="load"></div>
-	    </div>
+        </div>
+
         <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
             <div class="container">
                 <a class="navbar-brand" href="index.html">
                     <h1>FIND IT</h1>
                 </a>
-                
+
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="index.html">Home</a></li>
+                        <li class="active">
+                            <a href="index.html">Home</a>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav">
+                        <li class="active">
+                            <a href="index.html">Log out</a>
+                        </li>
                     </ul>
                 </div>
                 <!-- /.navbar-collapse -->
@@ -47,12 +56,13 @@
         
         <section id="intro" class="intro">
             <div class="slogan">
-                <h2>Register</h2>
-			     <h4>Join the community and help others to find their lost objects</h4>
+                <h2>Welcome!</h2>
+                <h4>It's good to see you back.</h4>
             </div>
             <div class="page-scroll">
             </div>
         </section>
+
         <!-- Section: services -->
         <section id="service" class="home-section text-center bg-gray">
             <div class="heading-about">
@@ -61,56 +71,44 @@
                         <div class="col-lg-8 col-lg-offset-2">
                             <div class="wow bounceInDown" data-wow-delay="0.4s">
                                 <div class="section-heading">
-                                    <h2>Register</h2>
+                                    <h2>Welcome!</h2>
                                     <i class="fa fa-2x fa-angle-down"></i>
+
                                 </div>
-                            </div>
+                           </div>
                         </div>
-                    </div>
+                     </div>
                 </div>
             </div>
             <div class="container">
                 <div class="row">
-                    <p>Insert your personal data to create a new profile</p>
                     <br>
-                    <br>
-                    <form class="block-form" method="post"          action="funciones.php">
-                        <input type="text"      name="name"             placeholder="Nombre"        id="name" required/>
-                        <input type="text"      name="mail"             placeholder="E-Mail"        id="mail" required/>
-                        <input type="password"  name="password"         placeholder="Password"      id="password" required/>
-                        <input type="password"  name="checkPassword"    placeholder="CheckPasword"  id="CheckPassword" required/>
-                        <?php 
-                            echo $_SESSION["Error"]; 
-                            $_SESSION["Error"] = "";
-                        ?>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <input type="submit" class="login centered-button" name="add" value="Register"/>
-                    </form>
-                </div>
+                    <?php 
+                        echo $_SESSION["Error"]; 
+                        $_SESSION["Error"] = "";
+                    ?>
+                </div>		
             </div>
         </section>
-        
         <footer>
             <div class="container">
                 <div class="row">
                     <div class="col-md-12 col-lg-12">
                         <p>&copy;Copyright 2015 -  All rights reserved.</p>
                     </div>
-                </div>
+                </div>	
             </div>
         </footer>
-        
+
         <!-- Core JavaScript Files -->
-        <script src="js/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/jquery.easing.min.js"></script>	
-	    <script src="js/jquery.scrollTo.js"></script>
-	    <script src="js/wow.min.js"></script>
+        <script src="js/jquery.min.js">         </script>
+        <script src="js/bootstrap.min.js">      </script>
+        <script src="js/jquery.easing.min.js">  </script>	
+        <script src="js/jquery.scrollTo.js">    </script>
+        <script src="js/wow.min.js">            </script>
         <!-- Custom Theme JavaScript -->
         <script src="js/custom.js"></script>
-        
+
     </body>
+
 </html>
